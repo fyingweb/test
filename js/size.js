5ec557d0a596235765ec05eb150ac3243ac61984
lying0525 <475161165@qq.com>
2018-06-07 14:42:44 +0800

    test

---
+++ js/size.js
@@ -0,0 +1,26 @@
+!function(e, t) {
+	function n() {
+		t.body ? t.body.style.fontSize = 12 * o + "px" : t.addEventListener("DOMContentLoaded", n)
+	}
+	function d() {
+		var e = (i.clientWidth > 920 ? 920 : i.clientWidth) / 10,
+			h = i.clientHeight;
+		i.style.fontSize = e + "px";
+		i.style.height = h +"px";
+	}
+	var i = t.documentElement,
+		o = e.devicePixelRatio || 1,
+		rat = i.clientHeight/i.clientWidth;
+	if (n(), d(), e.addEventListener("resize", d), e.addEventListener("pageshow", function(e) {
+		e.persisted && d();
+		alert(rat)
+		if(rat<1.7){
+			i.classList.add("other");
+			i.style.height = 'auto';
+		}
+	}), o >= 2) {
+		var a = t.createElement("body"),
+			s = t.createElement("div");
+		s.style.border = ".5px solid transparent", a.appendChild(s), i.appendChild(a), 1 === s.offsetHeight && i.classList.add("hairlines"), i.removeChild(a)
+	}
+}(window, document);
